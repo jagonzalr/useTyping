e163686687464dca9383464ca1e21d2a3776448b
Jose Antonio Gonzalez <jagonzalr@gmail.com>
2020-04-28 15:58:41 +0200

    Cleanup hook

--- demo/App.jsx
+++ demo/App.jsx
@@ -2,13 +2,27 @@
 
 import React from 'react'
 
+import { useTyping } from '../src/index'
+
 import './styles/tailwind.css'
 import './styles/index.scss'
 
+const WORDS = [
+  'I regret nothing.',
+  'Never listen to Pierce.',
+  'Cool cool cool.',
+  'Six seasons and a movie!'
+]
 export default () => {
+  const text = useTyping({ words: WORDS })
   return (
-    <div className='container mx-auto px-3 py-3'>
-      <h1 className='font-bold text-3xl flex-1'>useTyping</h1>
+    <div className='flex h-screen'>
+      <div className='m-auto'>
+        <h1 className='font-bold text-3xl flex-1'>useTyping</h1>
+        <p className='text-5xl' style={{ minHeight: '72px' }}>
+          {text}
+        </p>
+      </div>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
 'use strict'
+
+import { useEffect, useState, useRef } from 'react'
+
+export const useTyping = ({
+  words = [],
+  loop = true,
+  speed = 100,
+  speedAfterWord = 2000
+}) => {
+  const charIndexRef = useRef(-1)
+  const wordIndexRef = useRef(0)
+
+  const [charIndex, setCharIndex] = useState(-1)
+  const [wordIndex, setWordIndex] = useState(0)
+
+  useEffect(() => {
+    keystroke()
+  }, [])
+
+  useEffect(() => {
+    setCharIndex(charIndexRef.current)
+  }, [charIndexRef.current])
+
+  useEffect(() => {
+    setWordIndex(wordIndexRef.current)
+  }, [wordIndexRef.current])
+
+  const keystroke = async () => {
+    let nextDelay = speed
+    if (charIndexRef.current === words[wordIndexRef.current].length - 1) {
+      nextDelay = speedAfterWord
+      charIndexRef.current = -1
+      if (wordIndexRef.current === words.length - 1) {
+        if (!loop) return
+        wordIndexRef.current = 0
+      } else {
+        wordIndexRef.current = wordIndexRef.current + 1
+      }
+    } else {
+      charIndexRef.current = charIndexRef.current + 1
+    }
+
+    await delay(nextDelay)
+    setCharIndex(charIndexRef.current)
+    setWordIndex(wordIndexRef.current)
+    keystroke()
+  }
+
+  if (words && words.length > 0 && charIndex > -1) {
+    return words[wordIndex].slice(0, charIndex + 1)
+  }
+
+  return ''
+}
+
+// https://redux-saga.js.org/docs/introduction/BeginnerTutorial.html
+const delay = (ms = 3000) => new Promise(res => setTimeout(res, ms))
