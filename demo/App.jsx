918bc41ea7ab4083320e7c788ae619a63dda30e0
José Antonio González <jagonzalr@users.noreply.github.com>
2020-04-28 08:23:00 +0200

    Add demo (#2)

---
+++ demo/App.jsx
@@ -0,0 +1,14 @@
+'use strict'
+
+import React from 'react'
+
+import './styles/tailwind.css'
+import './styles/index.scss'
+
+export default () => {
+  return (
+    <div className='container mx-auto px-3 py-3'>
+      <h1 className='font-bold text-3xl flex-1'>useTyping</h1>
+    </div>
+  )
+}
