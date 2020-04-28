8f90a0442d06fc71e2d71286c42a5756346b4bb2
José Antonio González <jagonzalr@users.noreply.github.com>
2020-04-28 16:14:19 +0200

    Hook (#3)
    * Cleanup hook
    
    * Bump version
    
    * Rename to phrases
    
    * Update README.md
    
    * Remove dependency

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
 'use strict'
+
+import { useEffect, useState, useRef } from 'react'
+
+export const useTyping = ({
+  phrases = [],
+  loop = true,
+  speed = 100,
+  pause = 2000
+}) => {
+  const charIndexRef = useRef(-1)
+  const phraseIndexRef = useRef(0)
+
+  const [charIndex, setCharIndex] = useState(-1)
+  const [phraseIndex, setPhraseIndex] = useState(0)
+
+  useEffect(() => {
+    keystroke()
+  }, [])
+
+  useEffect(() => {
+    setCharIndex(charIndexRef.current)
+  }, [charIndexRef.current])
+
+  useEffect(() => {
+    setPhraseIndex(phraseIndexRef.current)
+  }, [phraseIndexRef.current])
+
+  const keystroke = async () => {
+    let nextDelay = speed
+    if (charIndexRef.current === phrases[phraseIndexRef.current].length - 1) {
+      nextDelay = pause
+      charIndexRef.current = -1
+      if (phraseIndexRef.current === phrases.length - 1) {
+        if (!loop) return
+        phraseIndexRef.current = 0
+      } else {
+        phraseIndexRef.current = phraseIndexRef.current + 1
+      }
+    } else {
+      charIndexRef.current = charIndexRef.current + 1
+    }
+
+    await delay(nextDelay)
+    setCharIndex(charIndexRef.current)
+    setPhraseIndex(phraseIndexRef.current)
+    keystroke()
+  }
+
+  if (phrases && phrases.length > 0 && charIndex > -1) {
+    return phrases[phraseIndex].slice(0, charIndex + 1)
+  }
+
+  return ''
+}
+
+// https://redux-saga.js.org/docs/introduction/BeginnerTutorial.html
+const delay = (ms = 3000) => new Promise(res => setTimeout(res, ms))
